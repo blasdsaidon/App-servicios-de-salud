53127c8cf9aecb0de10b4f3e3d537fa1d338582d
crisnov <beatlescris@gmail.com>
2023-06-30 16:42:43 -0300

    avance controladores imagen

--- src/main/java/com/proyectofinal/salud/controladores/pacienteControlador.java
+++ src/main/java/com/proyectofinal/salud/controladores/pacienteControlador.java
@@ -1,14 +1,19 @@
 package com.proyectofinal.salud.controladores;
 
+import com.proyectofinal.salud.entidades.paciente;
 import com.proyectofinal.salud.enumeradores.obraSocial;
 import com.proyectofinal.salud.enumeradores.sexo;
+import com.proyectofinal.salud.excepciones.MiException;
 import com.proyectofinal.salud.servicios.pacienteServicio;
 import java.util.Date;
 import java.util.List;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,33 +23,33 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 @RequestMapping("/paciente")
 public class pacienteControlador {
-    
+
     @Autowired
     private pacienteServicio pacienteServicio;
-    
+
     @GetMapping("/registrar")
-    public String registrar(ModelMap modelo){
-        
+    public String registrar(ModelMap modelo) {
+
         List<obraSocial> ListaOS = pacienteServicio.listadoObrasSocial();
         modelo.addAttribute("ListaOS", ListaOS);
         List<sexo> ListaGenero = pacienteServicio.listadoGeneros();
         modelo.addAttribute("ListaGenero", ListaGenero);
-        
+
         return "registro.html";
     }
-    
+
     @PostMapping("/registroPaciente")
-    public String registroPaciente(@RequestParam String nombre,@RequestParam String apellido,
-            @RequestParam String email,@RequestParam String telefono,@RequestParam(required = false) obraSocial obraSocial,
-            @RequestParam(required = false) sexo genero,@RequestParam String fechaNacimiento,@RequestParam String password,
-            @RequestParam String password2,@RequestParam(required = false) MultipartFile archivo,
-            ModelMap modelo, RedirectAttributes redireccion){
-    
-         try {
+    public String registroPaciente(@RequestParam String nombre, @RequestParam String apellido,
+            @RequestParam String email, @RequestParam String telefono, @RequestParam(required = false) obraSocial obraSocial,
+            @RequestParam(required = false) sexo genero, @RequestParam String fechaNacimiento, @RequestParam String password,
+            @RequestParam String password2, @RequestParam(required = false) MultipartFile archivo,
+            ModelMap modelo, RedirectAttributes redireccion) {
+
+        try {
             pacienteServicio.crearPaciente(nombre, apellido, email, telefono, obraSocial, genero, fechaNacimiento, password, password2, archivo);
             redireccion.addAttribute("exito", "El usuario se registró exitosamente!");
             return "redirect:/";// queda sujeto a cambio de front. 
-            
+
         } catch (Exception ex) {
             List<obraSocial> ListaOS = pacienteServicio.listadoObrasSocial();
             modelo.addAttribute("ListaOS", ListaOS);
@@ -56,8 +61,43 @@ public class pacienteControlador {
             modelo.put("telefono", telefono);
             modelo.put("email", email);
             modelo.put("fechaNacimiento", fechaNacimiento);
-            
+
             return "registro.html";
-        }  
+        }
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN', 'ROLE_PROFESIONAL')")
+    @GetMapping("/perfil")
+    public String perfil(ModelMap modelo, HttpSession session) {
+        paciente paciente = (paciente) session.getAttribute("usuariosession");
+        modelo.put("paciente", paciente);
+        return "registro.html";
+        
+        //inicio.html
+    }
+    
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN', 'ROLE_PROFESIONAL')")
+    @PostMapping("/perfil/{id}")
+    public String actualizar(MultipartFile archivo,@PathVariable String id, @RequestParam String nombre,@RequestParam String email, 
+            @RequestParam String password,@RequestParam String password2, ModelMap modelo) {
+
+        try {
+            pacienteServicio.modificarPaciente(password, nombre, email, email, password, obraSocial.OSEP, sexo.OTRO, email, password, password2, archivo);
+            
+//            actualizar(archivo, id, nombre, email, password, password2)
+
+            modelo.put("exito", "Paciente actualizado correctamente!");
+
+            return "inicio.html";
+            
+        } catch (MiException ex) {
+
+            modelo.put("error", ex.getMessage());
+            modelo.put("nombre", nombre);
+            modelo.put("email", email);
+
+            return "registro.html";
+        }
+
     }
 }
--- src/main/java/com/proyectofinal/salud/servicios/imagenServicio.java
+++ src/main/java/com/proyectofinal/salud/servicios/imagenServicio.java
@@ -3,8 +3,11 @@ package com.proyectofinal.salud.servicios;
 import com.proyectofinal.salud.entidades.imagen;
 import com.proyectofinal.salud.excepciones.MiException;
 import com.proyectofinal.salud.repositorios.imagenRepositorio;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -26,5 +29,39 @@ public class imagenServicio {
             }
         }
         return null;
+    }
+
+    public imagen actualizar(MultipartFile archivo, String idImagen) throws MiException {
+        if (archivo != null) {
+            try {
+
+                imagen imagen = new imagen();
+
+                if (idImagen != null) {
+                    Optional<imagen> respuesta = imagenRepositorio.findById(idImagen);
+
+                    if (respuesta.isPresent()) {
+                        imagen = respuesta.get();
+                    }
+                }
+
+                imagen.setMime(archivo.getContentType());
+
+                imagen.setNombre(archivo.getName());
+
+                imagen.setArchivo(archivo.getBytes());
+
+                return imagenRepositorio.save(imagen);
+
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return null;
+    }
+
+    @Transactional(readOnly = true)
+    public List<imagen> listarTodos() {
+        return imagenRepositorio.findAll();
     }
 }
