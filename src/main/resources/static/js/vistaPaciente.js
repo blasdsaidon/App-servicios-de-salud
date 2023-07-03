c2e3c9f603c99c4582b1da55ce81aa8ceac0b068
dpicco91 <dpicco91@hotmail.com>
2023-07-03 17:15:59 -0300

    vista paciente

---
+++ src/main/resources/static/js/vistaPaciente.js
@@ -0,0 +1,15 @@
+
+        function modificarDatos() {
+            // Lógica para modificar los datos personales del paciente
+            console.log('Modificar datos personales');
+        }
+
+        function sacarTurno() {
+            // Lógica para solicitar turno con el médico
+            console.log('Solicitar turno con el médico');
+        }
+
+        function verTurnos() {
+            // Lógica para ver los turnos del paciente
+            console.log('Ver turnos');
+        }
